51146451311f23136ce40cb8dcafe4cf811983d2
Konstantin <77053754+constzz@users.noreply.github.com>
2023-12-12 15:38:19 +0200

    Update implementation to new swift and new video graivty

--- Source/BMPlayerProtocols.swift
+++ Source/BMPlayerProtocols.swift
@@ -9,22 +9,22 @@
 import UIKit
 
 extension BMPlayerControlView {
-    public enum ButtonType: Int {
-        case play       = 101
-        case pause      = 102
-        case back       = 103
-        case fullscreen = 105
-        case replay     = 106
-    }
+	public enum ButtonType: Int {
+		case play = 101
+		case pause = 102
+		case back = 103
+		case fullscreen = 105
+		case replay = 106
+	}
 }
 
 extension BMPlayer {
-    static func formatSecondsToString(_ seconds: TimeInterval) -> String {
-        if seconds.isNaN {
-            return "00:00"
-        }
-        let min = Int(seconds / 60)
-        let sec = Int(seconds.truncatingRemainder(dividingBy: 60))
-        return String(format: "%02d:%02d", min, sec)
-    }
+	static func formatSecondsToString(_ secounds: TimeInterval) -> String {
+		if secounds.isNaN {
+			return "00:00"
+		}
+		let Min = Int(secounds / 60)
+		let Sec = Int(secounds.truncatingRemainder(dividingBy: 60))
+		return String(format: "%02d:%02d", Min, Sec)
+	}
 }
